79f1939b21992012f5289406f394f802ab2f570b
mikeill <mike@mzoo.org>
2024-07-11 16:20:35 -0500

    Fix up logic.

--- ashirah2024.py
+++ ashirah2024.py
@@ -124,37 +124,38 @@ if len(filelist) == 0:
 else:
     latest_file = sorted(filelist)[-1]
     with open(latest_file, "r") as file:
+      print("Opening latest file ", latest_file)
       workbooks = file.readlines()
-      for i in range(1, len(workbooks)):
-          workbook = workbooks[i].strip().split("\t")
-          if len(workbook) < 5:
-              continue
-          workbooks[i] = {
-              "title": workbook[0],
-              "workpages": int(workbook[1]),
-              "completed": int(workbook[2]),
-              "remaining": int(workbook[3]),
-              "completion": workbook[4]
-          }
-
       if not overview:
+          print(len(workbooks))
           with open(f"ashirah/{today}.csv", "w") as file:
             file.write("Title\tWork Pages\tCompleted\tRemaining\tCompletion\n")
-          completed = int(input(f"Pages in {workbooks[i]['title']} today? "))
-          if completed > 0:
+            for i in range(1, len(workbooks)):
+              workbook = workbooks[i].strip().split("\t")
+              if len(workbook) < 5:
+                  continue
+              completed = int(input(f"Pages in {workbook[0]} today? "))
+              print(workbook)
+              workbook = {
+                  "title": workbook[0],
+                  "workpages": int(workbook[1]),
+                  "completed": int(workbook[2]),
+                  "remaining": int(workbook[3]),
+                  "completion": workbook[4]
+              }
+              print(workbook)
               todays_work.append({
-                  "title": workbooks[i]["title"],
-                  "completed": f"{completed} page" + ("s" if completed > 1 else ""),
-              })
-
-              workbooks[i]["completed"] += completed
-              workbooks[i]["remaining"] -= completed
-              workbooks[i]["completion"] = str(math.floor(workbooks[i]["completed"] / workbooks[i]["workpages"] * 100)) + "%"
-              print(f"\n{random.choice(fun_unicode_characters)} Great job! Keep up the good work!\n")
-              file.write(f"{workbooks[i]['title']}\t{workbooks[i]['workpages']}\t{workbooks[i]['completed']}\t{workbooks[i]['workpages'] - workbooks[i]['completed']}\t{workbooks[i]['completion']}\n")
-          else:
-              print(f"\n{random.choice(fun_unicode_characters)} No worries! Keep up the good work!\n")
-              file.write(f"{workbooks[i]['title']}\t{workbooks[i]['workpages']}\t{workbooks[i]['completed']}\t{workbooks[i]['remaining']}\t{workbooks[i]['completion']}\n")
+                    "title": workbook["title"],
+                    "completed": f"{completed} page" + ("s" if completed > 1 else ""),
+                })
+              workbook["completed"] += completed
+              workbook["remaining"] -= completed
+              workbook["completion"] = str(math.floor(workbook["completed"] / workbook["workpages"] * 100)) + "%"
+              if completed > 0:
+                  print(f"\n{random.choice(fun_unicode_characters)} Great job! Keep up the good work!\n")
+              else:
+                  print(f"\n{random.choice(fun_unicode_characters)} No worries! Keep up the good work!\n")
+              file.write(f"{workbook['title']}\t{workbook['workpages']}\t{workbook['completed']}\t{workbook['remaining']}\t{workbook['completion']}\n")
           file.close()
           print("\nYour work has been saved!\n\n")
           print("\033[1mSummary of today's work:\033[0m\n")
@@ -164,6 +165,7 @@ else:
           meal = input(f"{random.choice(fun_unicode_characters)} {random.choice(fun_unicode_characters)} What are you thinking about meal on Friday? ")
           if (meal != ""):
               print(f"\nSounds good! Moving toward {meal} on Friday! {random.choice(fun_unicode_characters)} {random.choice(fun_unicode_characters)} {random.choice(fun_unicode_characters)}\n\n")
-      for workbook in workbooks[1:]:
-          print(f"\t{random.choice(fun_unicode_characters)} {workbook['title']}: {workbook['completion']} complete\n\t-----------------------------\n")
+      for book in workbooks[1:]:
+          workbook = book.strip().split("\t")
+          print(f"\t{random.choice(fun_unicode_characters)} {workbook[0]}: {workbook[4]} complete\n\t-----------------------------\n")
       print(f"\n\n{random.choice(fun_unicode_characters)} Getting there, homie!!! {random.choice(fun_unicode_characters)} Bye for now.  {random.choice(fun_unicode_characters)} {random.choice(fun_unicode_characters)}\n\n")
